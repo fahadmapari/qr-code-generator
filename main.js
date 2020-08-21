2c134040929aa53081a6e52a345a2511094d5627
fahad <fahadmapari09@gmail.com>
2020-08-21 13:33:41 +0530

    done

---
+++ main.js
@@ -0,0 +1,57 @@
+const gen = document.getElementById("generate");
+const qrData = document.getElementById("qr-input");
+const print = document.getElementById("print");
+const canvas = document.querySelector("canvas");
+const link = document.getElementById("link");
+const message = document.querySelector(".message");
+
+function isCanvasBlank(canvas) {
+  const context = canvas.getContext("2d");
+
+  const pixelBuffer = new Uint32Array(
+    context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
+  );
+
+  return !pixelBuffer.some((color) => color !== 0);
+}
+
+function showPopUp(msg, action) {
+  message.innerHTML = msg;
+
+  if (action) {
+    message.classList.add("message-container");
+  } else {
+    message.classList.add("message-container");
+    message.style.backgroundColor = "#ff3838";
+  }
+  setTimeout(() => {
+    message.classList.remove("message-container");
+    message.innerHTML = "";
+    message.style.backgroundColor = "";
+  }, 1500);
+}
+
+gen.addEventListener("click", (e) => {
+  if (qrData.value) {
+    QRCode.toCanvas(canvas, qrData.value, { width: 2000 }, function (error) {
+      if (error) showPopUp(error, false);
+      showPopUp("qr code successfully generated", true);
+      qrData.value = "";
+    });
+  } else {
+    showPopUp("please enter something", false);
+  }
+});
+
+print.addEventListener("click", (e) => {
+  if (isCanvasBlank(canvas)) {
+    showPopUp("please generate qr code first", false);
+  } else {
+    link.setAttribute("download", `qr_code_${Date.now().toString()}.png`);
+    link.setAttribute(
+      "href",
+      canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
+    );
+    link.click();
+  }
+});
